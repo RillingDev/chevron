6b32e897acb4e3e030d41faf75b8d21ef17f124d
TheBinaryBee <coolicooli1@hotmail.de>
2016-12-14 10:20:47 +0100

    updated stack

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,6 @@ const task_bundle_common = require("./tasks/bundle-common");
 const task_bundle_es = require("./tasks/bundle-es");
 const task_uglify = require("./tasks/uglify");
 const task_clean = require("./tasks/clean");
-const task_sync = require("./tasks/sync");
 const task_test = require("./tasks/test");
 
 gulp.task("bundle-browser", [], task_bundle_browser);
@@ -16,7 +15,6 @@ gulp.task("bundle-common", [], task_bundle_common);
 gulp.task("bundle-es", [], task_bundle_es);
 gulp.task("uglify", [], task_uglify);
 gulp.task("clean", [], task_clean);
-gulp.task("sync", [], task_sync);
 gulp.task("test", [], task_test);
 
 gulp.task("bundle", ["bundle-browser", "bundle-common", "bundle-es"]);
@@ -24,7 +22,7 @@ gulp.task("build", function(cb) {
     gulpSequence("clean", "bundle", "uglify", cb);
 });
 gulp.task("dist", function(cb) {
-    gulpSequence("sync", "build", "test", cb);
+    gulpSequence("build", "test", cb);
 });
 gulp.task("watch", function() {
     gulp.watch("./src/**/*.js", ["bundle"]);
