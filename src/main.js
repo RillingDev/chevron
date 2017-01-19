925caaebdbc39108bdad86988a92bbb813c7f16b
TheBinaryBee <coolicooli1@hotmail.de>
2017-01-19 10:20:59 +0100

    updated docs

--- src/main.js
+++ src/main.js
@@ -3,14 +3,16 @@
 import typeService from "./types/service";
 import typeFactory from "./types/factory";
 
-import construct from "./lib/construct";
-
 /**
- * Chevron Constructor
- * @constructor
- * @returns {Object} Chevron instance
+ * Chevron Class
+ * @class
  */
 const ChevronMain = class {
+    /**
+     * Chevron Constructor
+     * @constructor
+     * @returns {Chevron} Chevron instance
+     */
     constructor() {
         const _this = this;
 
@@ -21,10 +23,13 @@ const ChevronMain = class {
         _this.extend("service", typeService);
         _this.extend("factory", typeFactory);
     }
+    /**
+     * Defines a new module type
+     * @param {String} typeName name of the new type
+     * @param {Function} constructorFunction function init modules with
+     * @returns {Chevron} Chevron instance
+     */
     extend(typeName, constructorFunction) {
-        /**
-         * Defines a new module type
-         */
         const _this = this;
 
         //stores type with name into instance
@@ -34,29 +39,57 @@ const ChevronMain = class {
 
         return _this;
     }
-    provider(id, deps, fn, constructorFunction) {
-        /**
-         * Adds a new module to the container
-         */
+    /**
+     * Defines a new module
+     * @param {String} moduleName name of the module
+     * @param {Array} deps array of dependency names
+     * @param {Function} fn module content
+     * @param {Function} constructorFunction function init the modules with
+     * @returns {Chevron} Chevron instance
+     */
+    provider(moduleName, deps, fn, constructorFunction) {
         const _this = this;
         const _module = {
             deps,
             fn,
             rdy: false,
+            /**
+             * Inits the module
+             * @returns {Mixed} Module content
+             */
             init: function () {
-                return construct(_this.$map, _module, constructorFunction);
+                const dependencies = [];
+
+                //Collects dependencies
+                _module.deps.forEach(depName => {
+                    const dependency = _this.$map.get(depName);
+
+                    if (dependency) {
+                        dependencies.push(dependency.rdy ? dependency.fn : dependency.init());
+                    } else {
+                        throw new Error(`Missing '${depName}'`);
+                    }
+                });
+
+                //Calls constructorFunction on the module
+                _module.fn = constructorFunction(_module.fn, dependencies);
+                _module.rdy = true;
+
+                return _module.fn;
             }
         };
 
-        _this.$map.set(id, _module);
+        _this.$map.set(moduleName, _module);
 
         return _this;
     }
-    access(id) {
-        /**
-         * Accesses and inits a module
-         */
-        const _module = this.$map.get(id);
+    /**
+     * Access and init a module
+     * @param {String} moduleName name of the module to access
+     * @returns {Mixed} module content
+     */
+    access(moduleName) {
+        const _module = this.$map.get(moduleName);
 
         return _module.rdy ? _module.fn : _module.init();
     }
