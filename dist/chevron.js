b42b1154be18e75809811068117fbc18285ecf57
FelixRilling <contact@rilling.dev>
2019-11-17 22:04:01 +0100

    started adding jsdoc

--- dist/chevron.js
+++ dist/chevron.js
@@ -90,6 +90,36 @@ var chevron = (function (exports, lodash) {
         constructor() {
             this.injectables = new Map();
         }
+        /**
+         * Registers a new injectable on this container.
+         *
+         * @param initializer Initial value of this injectable. This can be any value, but usually  a class or a different kind of function.
+         *      During retrieval, the initial value might be transformed by the bootstrapper (see {@link Bootstrapping} for details).
+         *      If no name is provided in the options (see description of the options parameter, section "name"),
+         *      a name will be determined from the initializer through {@link getName}.
+         * @param dependencies Definitions of this injectables dependencies. Values can be either plain strings ("MyOtherService"),
+         *      or a value which is nameable. For details on nameable values see {@link getName}.
+         * @param options Options for this injectable. The following options exist:
+         *      <ul>
+         *          <li>name:
+         *                  Name for this injectable. If this is not provided, the name will be determined based on the initializer.
+         *                  (see description of the initializer parameter)
+         *          </li>
+         *          <li>bootstrapping:
+         *                  Bootstrapping strategy to use when instantiating this injectable (see {@link Bootstrapping} for details).
+         *                  By default, {@link DefaultBootstrappings.IDENTITY} is used. If your injectable is a class or factory function,
+         *                  consider using {@link DefaultBootstrappings.CLASS} or {@link DefaultBootstrappings.FUNCTION} instead respectively,
+         *                  or provide your own.
+         *          </li>
+         *          <li>scope:
+         *                  Scoping strategy to use when retrieving instances (see {@link Scope} for details).
+         *                  By default, {@link DefaultScopes.SINGLETON} is used. For different use cases,
+         *                  see {@link DefaultScopes.PROTOTYPE} or provide your own.
+         *          </li>
+         *      </ul>
+         * @throws Error when an injectable with the requested name is already registered.
+         * @throws TypeError when no name can be determined for this injectable or any of its dependencies.
+         */
         registerInjectable(initializer, dependencies, options = {}) {
             const { bootstrapping, scope, name } = lodash.defaults(options, {
                 bootstrapping: DefaultBootstrappings.IDENTITY,
@@ -110,10 +140,30 @@ var chevron = (function (exports, lodash) {
                 instances: new Map()
             });
         }
+        /**
+         * Checks if an injectable with the name provided is registered for this container, regardless if its instantiated or not.
+         * To check if an injectable is registered and instantiated, see {@link #hasInjectableInstance}.
+         *
+         * @param name Either a raw string name or a nameable value that should be checked for. See {@link #registerInjectable} for details.
+         * @return if an injectable with the name provided is registered on this container.
+         * @throws TypeError when no name can be determined for the provided nameable.
+         */
         hasInjectable(name) {
             return this.injectables.has(getInjectableName(name));
         }
+        /**
+         * Checks if an injectable with the name provided is registered and instantiated for this container.
+         * To check if an injectable is registered without checking for instantiation, see {@link #hasInjectable}.
+         *
+         * @param name Either a raw string name or a nameable value that should be checked for. See {@link #registerInjectable} for details.
+         * @param context Context to be used for instance checks. See {@link Scope} for details.
+         * @return if an injectable with the name provided is registered and instantiated on this container.
+         * @throws TypeError when no name can be determined for the provided nameable.
+         */
         hasInjectableInstance(name, context = null) {
+            if (!this.hasInjectable(name)) {
+                return false;
+            }
             const { injectableEntry, instanceName } = this.resolveInjectableInstance(name, context);
             return (instanceName != null && injectableEntry.instances.has(instanceName));
         }
