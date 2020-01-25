f26b7980ea7ad11302838e152f6c8b68913e8626
FelixRilling <contact@rilling.dev>
2020-01-25 13:57:04 +0100

    Updated stack

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,15 @@
-{
-    "parser": "@typescript-eslint/parser",
-    "plugins": [
+module.exports = {
+    root: true,
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ["./src/tsconfig.json", "./spec/src/tsconfig.json"]
+    },
+    plugins: [
         "@typescript-eslint",
         "prettier"
     ],
-    "extends": [
+    extends: [
         "eslint:recommended",
         "plugin:@typescript-eslint/eslint-recommended",
         "plugin:@typescript-eslint/recommended",
@@ -12,46 +17,44 @@
         "prettier",
         "prettier/@typescript-eslint"
     ],
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module",
-        "project": "./tsconfig.lint.json"
-    },
-    "env": {
+    env: {
         "browser": true,
-        "es6": true,
         "node": true
     },
-    "rules": {
-        "capitalized-comments": "warn",
-        "curly": [
-            "warn",
-            "all"
-        ],
-        "no-extra-parens": "off",
+    rules: {
+        "no-shadow": "error",
+        "no-undef-init": "error",
+        "no-import-assign": "error",
         "no-template-curly-in-string": "warn",
         "array-callback-return": "warn",
         "consistent-return": "warn",
         "no-else-return": "warn",
         "no-implicit-coercion": "warn",
-        "no-shadow": "error",
-        "no-undef-init": "warn",
+        "no-dupe-else-if": "warn",
         "prettier/prettier": "warn",
-        "@typescript-eslint/explicit-member-accessibility": "warn",
+        "@typescript-eslint/no-floating-promises": "error",
+        "@typescript-eslint/no-throw-literal": "error",
         "@typescript-eslint/interface-name-prefix": [
             "warn",
             {
                 "prefixWithI": "never"
             }
         ],
-        "@typescript-eslint/no-floating-promises": "error",
         "@typescript-eslint/consistent-type-assertions": [
             "warn",
             {
                 "assertionStyle": "angle-bracket"
             }
         ],
+        "@typescript-eslint/no-dynamic-delete": "warn",
+        "@typescript-eslint/no-extra-non-null-assertion": "warn",
+        "@typescript-eslint/no-implied-eval": "warn",
+        "@typescript-eslint/no-non-null-asserted-optional-chain": "warn",
+        "@typescript-eslint/no-unnecessary-condition": "warn",
+        "@typescript-eslint/prefer-nullish-coalescing": "warn",
+        "@typescript-eslint/prefer-optional-chain": "warn",
+        "@typescript-eslint/prefer-readonly": "warn",
         "@typescript-eslint/no-explicit-any": "off",
         "@typescript-eslint/no-non-null-assertion": "off"
     }
-}
+};
